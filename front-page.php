4810c24743f8b3ec7eb52c399e908022300d5a38
Krosnoz <meufiearth@gmail.com>
2020-11-13 16:43:06 +0100

    New

--- front-page.php
+++ front-page.php
@@ -37,8 +37,7 @@
             </footer>
         </div>
 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
         <?php wp_footer(); ?>
 </body>
 
