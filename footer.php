fe8f42dd79317997bbd868552bccb1450dc0208d
Krosnoz <meufiearth@gmail.com>
2020-11-13 16:42:12 +0100

    New

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
 
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	
+	<!-- Ici on pourra rajouter nos plugins JS -->
+
 	<?php wp_footer(); ?>
 </body>
 
