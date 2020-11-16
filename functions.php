546809496316fecc5d185ca22a2f33abdb3c97e2
Krosnoz <meufiearth@gmail.com>
2020-11-16 09:41:03 +0100

    New

--- functions.php
+++ functions.php
@@ -1,18 +1,17 @@
 <?php 
 
 //Permet d'enregister des menus pour que le client les modifies, et les classes CSS
-register_nav_menus( array(
-	'menu-home' => 'Menu Accueil',
-	'menu-reseaux' => 'Menu Réseaux Sociaux'
-) );
+function custom_nav_menus() {
+	register_nav_menus( array(
+		'menu-home' => 'Menu Accueil',
+		'menu-reseaux' => 'Menu Réseaux Sociaux'
+	) );
+}
+add_action( '1', 'custom_nav_menus' );
 
 //Permet d'enregister le style.css + bootstrap pour ceux qui l'utiliseront
-function wpdocs_theme_name_scripts() {
-	wp_register_style('main-style', get_template_directory_uri().'/style.css', array(), true);
-	wp_enqueue_style('main-style');
-	wp_register_style('bootstrap-style', 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css', array(), true);
-	wp_enqueue_style('bootstrap-style');
+function custom_theme_assets() {
+	wp_enqueue_style('main-style', get_template_directory_uri().'/style.css');
+	wp_enqueue_style('bootstrap-style', 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css');
 }
-add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts');
-
-?>
+add_action( 'wp_enqueue_scripts', 'custom_theme_assets');
